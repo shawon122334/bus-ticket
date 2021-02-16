0ebbf9906bf2cbf10cf8f7a0c269e542468ab16f
Sabbir Ahmed Shawon <shawon.mu11@gmail.com>
2021-02-17 02:00:49 +0600

    initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+// function plusMinus(isIncrease){
+//     const firstClassInput = document.getElementById("firstClassInput");
+//     const firstClassInputValue = parseInt(firstClassInput.value);
+//     let firstClassInputNewValue = 0;
+//     if (isIncrease == 'true') {
+//         firstClassInputNewValue = firstClassInputValue + 1;
+
+//     }
+//     if (isIncrease == 'false' && firstClassInputValue>0) {
+//         firstClassInputNewValue = firstClassInputValue - 1;
+
+//     }
+//     firstClassInput.value = firstClassInputNewValue;
+// }
+function plusMinus(isIncrease, product) {
+    const inputField = document.getElementById(product + "ClassInput");
+    const inputFieldValue = parseInt(inputField.value);
+    let inputFieldNewValue = 0;
+    if (isIncrease == 'true') {
+        inputFieldNewValue = inputFieldValue + 1;
+
+    }
+    if (isIncrease == 'false' && inputFieldValue > 0) {
+        inputFieldNewValue = inputFieldValue - 1;
+
+    }
+    inputField.value = inputFieldNewValue;
+
+    // document.getElementById("subTotal").innerText='$ '+totalNewPrice;
+    calculateTotal();
+}
+
+
+
+
+
